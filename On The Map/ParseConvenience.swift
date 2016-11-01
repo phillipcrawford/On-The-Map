4c5391562f06b77854c031d52e8115191a2c30c8
[01;31m[K"name"[m[K: "Phillip Crawford <[01;31m[K"email"[m[K: "phillipcrawford3@gmail.com>
2016-11-01 15:09:53 -0700

    delete comments

--- On The Map/ParseConvenience.swift
+++ On The Map/ParseConvenience.swift
@@ -27,23 +27,6 @@ extension ParseClient {
         }
     }
     
-//    func postStudentInformation(completionHandlerForStudentInformation: (result: [StudentInformation]?, error: String?) -> Void) {
-//        let method = ParseClient.Methods.StudentLocation
-//        let jsonBody = "{\"\(ParseClient.JSONBodyKeys.UniqueKey)\": \"\(userID!)\", \"\(ParseClient.JSONBodyKeys.FirstName)\": \"\(firstName!)\", \"\(ParseClient.JSONBodyKeys.LastName)\": \"\(lastName!)\",\"\(ParseClient.JSONBodyKeys.MapString)\": \"\(mapString!)\", \"\(ParseClient.JSONBodyKeys.MediaURL)\": \"\(mediaURL!)\",\"\(ParseClient.JSONBodyKeys.Latitude)\": \(latitude!), \"\(ParseClient.JSONBodyKeys.Longitude)\": \(longitude!)}"
-//        taskForPOSTMethod(method, jsonBody: jsonBody) { (results, error) in
-//            if let error = error {
-//                completionHandlerForStudentInformation(result: nil, error: (error.userInfo)["NSLocalizedDescription"]! as? String)
-//            } else {
-//                if let results = results as? [String:AnyObject!] {
-//                    let currentStudent = StudentInformation.currentStudent(results)
-//                    completionHandlerForStudentInformation(result: currentStudent, error: nil)
-//                } else {
-//                    completionHandlerForStudentInformation(result: nil, error: "getStudentInformation parsing")
-//                }
-//            }
-//        }
-//    }
-    
     func postStudentInformation(completionHandlerForStudentInformation: (result: [StudentInformation]?, error: String?) -> Void) {
         let method = ParseClient.Methods.StudentLocation
         
